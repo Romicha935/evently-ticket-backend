8b814743e4285bf59d81556f92eb9a756bb2bd3c
Romicha Parvin <romichaparvin35@gmail.com>
2026-09-14 23:31:56 +0600

    jwt guard add

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { JwtAuthGuard } from './guards/jwt_auth.guards';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -22,4 +23,11 @@ export class AuthController {
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
+
+  @Get('profile')
+@UseGuards(JwtAuthGuard)
+@ApiOperation({ summary: 'Get current user profile' })
+getProfile(@Req() req: any) {
+  return req.user;
+}
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: {
@@ -13,6 +16,6 @@ import { AuthService } from './auth.service';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
