3134c6b7e56ae911cd177c345aee0aac0bc2e9d2
Romicha Parvin <romichaparvin35@gmail.com>
2026-09-15 21:35:52 +0600

    event crud successfull

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,26 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 
 @Injectable()
 export class EventsService {
-  create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createEventDto: CreateEventDto) {
+    return this.prisma.event.create({
+      data: {
+        title: createEventDto.title,
+        description: createEventDto.description,
+        image: createEventDto.image,
+        location: createEventDto.location,
+        date: new Date(createEventDto.date),
+        price: createEventDto.price,
+        capacity: createEventDto.capacity,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all events`;
+  async findAll() {
+    return this.prisma.event.findMany({
+      orderBy: {
+        date: 'asc',
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
+  async findOne(id: number) {
+    const event = await this.prisma.event.findUnique({
+      where: { id },
+    });
+
+    if (!event) {
+      throw new NotFoundException('Event not found');
+    }
+
+    return event;
   }
 
-  update(id: number, updateEventDto: UpdateEventDto) {
-    return `This action updates a #${id} event`;
+  async update(id: number, updateEventDto: UpdateEventDto) {
+    await this.findOne(id);
+
+    return this.prisma.event.update({
+      where: { id },
+      data: {
+        ...updateEventDto,
+        date: updateEventDto.date
+          ? new Date(updateEventDto.date)
+          : undefined,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} event`;
+  async remove(id: number) {
+    await this.findOne(id);
+
+    return this.prisma.event.delete({
+      where: { id },
+    });
   }
 }
